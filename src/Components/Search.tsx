1b7fb1781a79e9d324fb20418b94a4a98d8ba2ea
Hamba Allah <hambaallah@MacBook-Pro-MAC.local>
2023-06-30 21:30:33 +0700

    Adding daisyui and updating some UI on Search Section

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -13,7 +13,7 @@ const Search = () => {
         setBooks(data.books)
     }
 
-    const getBookByQuery = async (q) =>{
+    const getBookByQuery = async (q) => {
         let link = `https://www.dbooks.org/api/search/${q}`
         const raw = await fetch(link)
         const newBook = await raw.json()
@@ -28,29 +28,42 @@ const Search = () => {
 
     return (
         <div className="p-4 flex flex-col justify-start">
-            <input className="p-2 w-full" type="text" onChange={(e)=> setQuery(e.target.value)} placeholder="name of your book" />
-            <button className="font-bold p-2 m-2 bg-yellow-500" onClick={()=>getBookByQuery(query)}>Search this book</button>
+            <input type="text" placeholder="name of your book" onChange={(e) => setQuery(e.target.value)} className="input input-bordered w-full max-w-xs" />
+            <button className="font-bold p-2 m-2 btn" onClick={() => getBookByQuery(query)}>Search this book</button>
             {
-                total && <input className="p-2 w-full" type="text" onChange={(e)=> setSearch(e.target.value)} placeholder="found exact book you looking for!" />
+                total && <input className="input input-bordered w-full max-w-xs" type="text" onChange={(e) => setSearch(e.target.value)} placeholder="found exact book you looking for!" />
             }
             {
                 total ? <h1 className="text-lg font-bold p-2 rounded-full bg-yellow-500 w-fit m-2">Total: {total}</h1> :
-                 <h1>Your book is not found! or u just miss type ?</h1>
+                    <h1 className="btn" disabled="disabled">Your book is not found! or u just miss type ?</h1>
             }
-            {
-                books && books.filter(book=>{
-                    return search.toLocaleLowerCase() === '' ? book : book.title.toLowerCase().includes(search.toLocaleLowerCase())
-                }).map(book => {
-                    return (
-                        <ul className="list-disc list-inside" key={book.id}>
-                            <li>
-                                {book.title}
-                            </li>
-                        </ul>
-                    )
-                })
-            }
-        </div>
+            <h1>Your title book`s :</h1>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th className="text-lg">ID</th>
+                            <th className="text-lg">Title</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            books && books.filter(book => {
+                                return search.toLocaleLowerCase() === '' ? book : book.title.toLowerCase().includes(search.toLocaleLowerCase())
+                            }).map(book => {
+                                return (
+                                    <tr>
+                                        <th>{book.id}</th>
+                                        <td>{book.title}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div >
     )
 }
 
